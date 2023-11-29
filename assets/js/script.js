4b15a346b79d181b5a1acb938616d3bd320c765a
garrett lieb <liebg08@gmail.com>
2023-11-29 14:40:49 -0800

    java cleanup; need to fix local storage GET, seems like things are storing okay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,53 @@
 var currentDay = $("#currentDay");
 var saveBtn = $(".saveBtn");
-var Events = $(".fas fa-save");
+var events = $(".fas fa-save");
 var text = $("textarea");
 var time = $(".time-block").attr("id");
 var currentHour = dayjs().format("H");
 var hour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var textarray= [];
 var timearray= [];
-console.log("hey get to work");
+
+
+
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+
 
 
 $( document ).ready(function() {
+  console.log("hey get to work");
 
   $(".saveBtn").on("click", function() {
-    let time = $(this).parent().attr("id");
-    let text = $(this).siblings(".description").val();
-
-    timearray.push(time);
-    textarray.push(text);
-
-    console.log(textarray);
-    console.log(timearray);
+    //when the save button is clicked, the text in the textarea is saved to local storage
+    //the time is saved to local storage
+    //the text and time are saved to an array to be recalled when the page is refreshed
+    // use the id in the time id to 
+    console.log("save button clicked");
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
     console.log(text);
     console.log(time);
-
+    textarray.push(text);
+    timearray.push(time);
+    localStorage.setItem(timearray, textarray);
+    console.log(textarray);
+    console.log(timearray);
   });
+
+  //when the page is refreshed, the text in the textarea is recalled from local storage
+  //the time is recalled from local storage
+  //the text and time are recalled from the array
+  //the text are displayed in the textarea
+  //the time are displayed in the time block
+
+  //function init() {}
+
+
 
   function displayTime() {
     var rightNow = dayjs().format ( 'MMM DD, YYYY [at] hh:mm:ss A' );
@@ -42,9 +65,11 @@ $( document ).ready(function() {
     } 
   }
 
+  //init();
   renderBackground();
   setInterval(renderBackground, 1000);
   displayTime();
   setInterval(displayTime, 1000);
+  
 
 });
