50a583db6acb99ba407f0323d366430d44feb5e5
garrett lieb <liebg08@gmail.com>
2023-11-29 00:05:35 -0800

    java

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,12 +39,10 @@ $( document ).ready(function() {
     console.log("save button clicked");
 
   // get the user input from the textarea
-    var time = $(this).parent().attr("id");
-    var text = $(this).siblings(".description").val();
 
   //function to store the user input in local storage?
 
-    localStorage.setItem(time, text);
+    
     console.log(time);
     console.log(text);
     
@@ -89,26 +87,4 @@ $( document ).ready(function() {
     setInterval(displayTime, 1000);
 
   });
- 
- 
-
-
-  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-  // the code isn't run until the browser has finished rendering all the elements
-  // in the html.
-  //
-  //// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  //// TODO: Add code to display the current date in the header of the page.
 
