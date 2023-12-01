ca3d2856ab3a911a80803175521a7850ca128ef8
garrett lieb <liebg08@gmail.com>
2023-11-30 21:07:55 -0800

    combined var/let for timearray/textarray, seems like it fixed the issue with only keepiong text field for most recent reload

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,83 +5,60 @@ var text = $(".description");
 var time = $(".time-block").attr("id");
 var currentHour = dayjs().format("H");
 var hour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-var textarray= [];
-var timearray= [];
 
 
+  $(document).ready(function() {
+    console.log("hey get to work");
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+    var textarray = JSON.parse(localStorage.getItem("textarray")) || [];
+    var timearray = JSON.parse(localStorage.getItem("timearray")) || [];
 
+    $(".saveBtn").on("click", function() {
+      console.log("save button clicked");
+      var savedtext = $(this).siblings(".description").val();
+      var savedtime = $(this).parent().attr("id");
+      console.log(savedtext);
+      console.log(savedtime);
+      textarray.push(savedtext);
+      timearray.push(savedtime);
+      localStorage.setItem("timearray", JSON.stringify(timearray));
+      localStorage.setItem("textarray", JSON.stringify(textarray));
+      console.log(textarray);
+      console.log(timearray);
+    });
 
-$( document ).ready(function() {
-  console.log("hey get to work");
-
-  $(".saveBtn").on("click", function() {
-    console.log("save button clicked");
-    let savedtext = $(this).siblings(".description").val();
-    let savedtime = $(this).parent().attr("id");
-    console.log(savedtext);
-    console.log(savedtime);
-    textarray.push(savedtext);
-    timearray.push(savedtime);
-    localStorage.setItem("timearray", JSON.stringify(timearray));
-    localStorage.setItem("textarray", JSON.stringify(textarray));
-    console.log(textarray);
-    console.log(timearray);
-  });
-
-// get text from local storage and display it on the page
-
-
-  function getText() {
-    let timearray = JSON.parse(localStorage.getItem("timearray"));
-    console.log(timearray);
-    let textarray = JSON.parse(localStorage.getItem("textarray"));
-    console.log(textarray);
-
-    // Populate the "description" field based on the values in "textarray" in realtion to "timearray"
-    for (let i = 0; i < timearray.length; i++) {
-      let timeId = "#" + timearray[i];
-      let descriptionField = $(timeId).find(".description");
-      descriptionField.val(textarray[i]);
+    function getText() {
+      // Populate the "description" field based on the values in "textarray" in relation to "timearray"
+      for (var i = 0; i < timearray.length; i++) {
+        var timeId = "#" + timearray[i];
+        var descriptionField = $(timeId).find(".description");
+        descriptionField.val(textarray[i]);
+      }
     }
-  };
 
+    function displayTime() {
+      var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss A');
+      currentDay.text(rightNow);
+    }
 
+    function renderBackground() {
+      for (var i = 0; i < hour.length; i++)
+        if (hour[i] < currentHour) {
+          text[i].classList.add("past");
+        } else if (hour[i] > currentHour) {
+          text[i].classList.add("future");
+        } else {
+          text[i].classList.add("present");
+        }
+    }
 
-  
-
-  function displayTime() {
-    var rightNow = dayjs().format ( 'MMM DD, YYYY [at] hh:mm:ss A' );
-    currentDay.text(rightNow);
-  }
-
-
-  function renderBackground() {
-    for (var i = 0; i < hour.length; i++)
-    if (hour[i] < currentHour) {
-      text[i].classList.add("past");
-    } else if (hour[i] > currentHour) {
-      text[i].classList.add("future");
-    } else {
-      text[i].classList.add("present");
-    } 
-  }
-  
-  // when the page loads pull from local storage and display it on the page
-  
     function init() {
-    getText();
-    displayTime();
-    setInterval(displayTime, 1000);
-    renderBackground();
-    setInterval(renderBackground, 1000);
-    };
+      getText();
+      displayTime();
+      setInterval(displayTime, 1000);
+      renderBackground();
+      setInterval(renderBackground, 1000);
+    }
 
     init();
-});
+  });
